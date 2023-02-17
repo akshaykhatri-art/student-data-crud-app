dceb717fc9f45ea788ce1b780cc5f72f4bab97e6
akshaykhatri-art <akshaykhatri22@gmail.com>
2023-02-17 12:13:52 -0800

    Edit route added with method-override

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 const Student = require('./models/student');
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended: true}));
 
 // app.get('/register', (req, res) => {
 //     res.send('Register!!')
@@ -22,6 +19,12 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
 });
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 app.get('/students', async (req, res) => {
     const students = await Student.find({})
@@ -42,6 +45,18 @@ app.get('/students/:id', async (req, res) => {
     const {id} = req.params;
     const student = await Student.findById(id)
     res.render('students/show', {student})
+})
+
+app.get('/students/:id/edit', async(req, res) => {
+    const {id} = req.params;
+    const student = await Student.findById(id)
+    res.render('students/edit', {student})
+})
+
+app.put('/students/:id', async(req, res) => {
+    const {id} = req.params;
+    const student = await Student.findByIdAndUpdate(id, req.body, {runValidaters: true, new: true})
+    res.redirect(`/students/${student._id}`)
 })
 
 app.listen(3000, () => {
